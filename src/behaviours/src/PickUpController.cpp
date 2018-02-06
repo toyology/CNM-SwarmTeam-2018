a9c1e2938662106b1a1542296e44d136619453aa
toyology <slindsley@gmail.com>
2018-02-05 21:23:29 -0700

    Changed pickup.cpp values to match previous years values

--- src/behaviours/src/PickUpController.cpp
+++ src/behaviours/src/PickUpController.cpp
@@ -188,7 +188,7 @@ Result PickUpController::DoWork()
   if (!targetHeld)
   {
     //threshold distance to be from the target block before attempting pickup
-    float targetDistance = 0.15; //meters
+    float targetDistance = 0.14; //meters //ORIGINALLY .15 // .14 value from CNM 2017
 
     // -----------------------------------------------------------
     // millisecond time = current time if not in a counting state
@@ -219,19 +219,19 @@ Result PickUpController::DoWork()
     // The sequence of events is:
     // 1. Target aquisition phase: Align the robot with the closest visible target cube, if near enough to get a target lock then start the pickup timer (Td)
     // 2. Approach Target phase: until *grasp_time_begin* seconds
-    // 3. Stop and close fingers (hopefully on a block - we won't be able to see it remember): at *grasp_time_begin* seconds 
-    // 4. Raise the gripper - does the rover see a block or did it miss it: at *raise_time_begin* seconds 
+    // 3. Stop and close fingers (hopefully on a block - we won't be able to see it remember): at *grasp_time_begin* seconds
+    // 4. Raise the gripper - does the rover see a block or did it miss it: at *raise_time_begin* seconds
     // 5. If we get here the target was not seen in the robots gripper so drive backwards and and try to get a new lock on a target: at *target_require_begin* seconds
     // 6. If we get here we give up and release control with a task failed flag: for *target_pickup_task_time_limit* seconds
-    
+
     // If we don't see any blocks or cubes turn towards the location of the last cube we saw.
     // I.E., try to re-aquire the last cube we saw.
 
-    float grasp_time_begin = 1.5;
-    float raise_time_begin = 2.0;
-    float lower_gripper_time_begin = 4.0;
-    float target_reaquire_begin= 4.2;
-    float target_pickup_task_time_limit = 4.8;
+    float grasp_time_begin = 1.2; //meters //ORIGINALLY 1.5 // 1.2 value from CNM 2017
+    float raise_time_begin = 1.5; //meters //ORIGINALLY 2.0 // 1.5 value from CNM 2017
+    float lower_gripper_time_begin = 2.6; //meters //ORIGINALLY 4.0
+    float target_reaquire_begin= 2.5; //meters //ORIGINALLY 4.2 // 2.5 value from CNM 2017
+    float target_pickup_task_time_limit = 5.0; //meters //ORIGINALLY 4.8 // 5.0 value from CNM 2017
 
     //cout << "blockDistance DOWORK:  " << blockDistance << endl;
 
@@ -248,7 +248,7 @@ Result PickUpController::DoWork()
         nTargetsSeen = 0;
     }
 
-    
+
     if (nTargetsSeen == 0 && !lockTarget)
     {
       // This if statement causes us to time out if we don't re-aquire a block within the time limit.
@@ -298,7 +298,7 @@ Result PickUpController::DoWork()
     }
     else if (Td > raise_time_begin) //raise the wrist
     {
-      result.pd.cmdVel = -0.15;
+      result.pd.cmdVel = -0.25; //meters //ORIGINALLY -0.15 // -0.25 value from CNM 2017
       result.pd.cmdAngularError= 0.0;
       result.wristAngle = 0;
     }
